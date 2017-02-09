15f548114f913f3330fe646cee12d41e4e5141cc
Bruno Portable <galliez.bruno@gmail.com>
2017-02-09 14:33:19 +0100

    connerie

--- test1/test1/Form1.cs
+++ test1/test1/Form1.cs
@@ -12,6 +12,15 @@ namespace test1 {
     public partial class Form1 : Form {
         public Form1 () {
             InitializeComponent();
+            // connerie 1
+            // connerie 2
+            // connerie 3
+            // connerie 4
+
+            // connerie 1
+            // connerie 2
+            // connerie 3
+            // connerie 4
         }
 
         private void Form1_Load ( object sender , EventArgs e ) {
