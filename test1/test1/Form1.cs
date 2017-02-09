9ba220ed1d9aa1bba712100a121bfc9100b3eb70
nicol <nicol@HAKABOB>
2017-02-09 14:34:05 +0100

    Commentaires

--- test1/test1/Form1.cs
+++ test1/test1/Form1.cs
@@ -12,6 +12,10 @@ namespace test1 {
     public partial class Form1 : Form {
         public Form1 () {
             InitializeComponent();
+            //Chauvequipeut
+            //Blav ozico izcezv nae f d
+            //veldj ebm ùlkanvm ajrv oa"jbvr lkeav na
+            // aovd vojvb ejn bornbaer nba
         }
 
         private void Form1_Load ( object sender , EventArgs e ) {
