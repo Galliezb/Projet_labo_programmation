c1cb08df170b2477f1987305590e3288cb78765f
nicol <nicol@HAKABOB>
2017-03-19 16:36:56 +0100

    test interface

--- test1/test1/Form1.Designer.cs
+++ test1/test1/Form1.Designer.cs
@@ -24,24 +24,19 @@
         /// </summary>
         private void InitializeComponent () {
             this.components = new System.ComponentModel.Container();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.hide = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.testpanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(180, 56);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(98, 21);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // contextMenuStrip1
             // 
@@ -55,49 +50,100 @@
             this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
             this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(103, 139);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(66)))));
+            this.panel1.Controls.Add(this.hide);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(62, 427);
+            this.panel1.TabIndex = 1;
             // 
-            // listBox1
+            // hide
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(106, 197);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 84);
-            this.listBox1.TabIndex = 3;
+            this.hide.BackColor = System.Drawing.Color.Transparent;
+            this.hide.Dock = System.Windows.Forms.DockStyle.Top;
+            this.hide.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.hide.FlatAppearance.BorderSize = 2;
+            this.hide.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.hide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hide.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hide.ForeColor = System.Drawing.Color.White;
+            this.hide.Location = new System.Drawing.Point(0, 0);
+            this.hide.Name = "hide";
+            this.hide.Size = new System.Drawing.Size(62, 103);
+            this.hide.TabIndex = 0;
+            this.hide.Text = "H\r\nI\r\nD\r\nE";
+            this.hide.UseVisualStyleBackColor = false;
+            this.hide.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(999, 83);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.Location = new System.Drawing.Point(400, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 70);
+            this.panel4.TabIndex = 0;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // testpanel
+            // 
+            this.testpanel.BackColor = System.Drawing.Color.Teal;
+            this.testpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.testpanel.Location = new System.Drawing.Point(62, 83);
+            this.testpanel.Name = "testpanel";
+            this.testpanel.Size = new System.Drawing.Size(937, 100);
+            this.testpanel.TabIndex = 4;
+            this.testpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.testpanel_Paint);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(999, 510);
+            this.Controls.Add(this.testpanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.DirectoryServices.DirectorySearcher directorySearcher1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button hide;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel testpanel;
     }
 }
 
--- test1/test1/Form1.cs
+++ test1/test1/Form1.cs
@@ -7,27 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+    
+
 
 namespace test1 {
     public partial class Form1 : Form {
+        int op;
+        bool Hided;
+        
         public Form1 () {
             InitializeComponent();
-            //Chauvequipeut
-            //Blav ozico izcezv nae f d
-            //veldj ebm ùlkanvm ajrv oa"jbvr lkeav na
-            // aovd vojvb ejn bornbaer nba
-            // connerie 1
-            // connerie 2
-            // connerie 3
-            // connerie 4
-
-            // connerie 1
-            // connerie 2
-            // connerie 3
-            // connerie 4
+            testpanel.Visible = false;
+            Hided = true;
+            op = 0;
+         
         }
 
         private void Form1_Load ( object sender , EventArgs e ) {
+          
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            testpanel.BackColor = Color.FromArgb(op, Color.Teal);
+
+            if (Hided)
+            {
+                testpanel.Visible = true;
+                op += 4;
+                if (op == 252)
+                {
+                    timer1.Stop();
+                    Hided = false;
+                }
+                
+                //this.Refresh();
+
+            }
+            else
+            {
+
+                op -= 4;
+                if (op <= 4)
+                {
+                    testpanel.Visible = false;
+                    timer1.Stop();
+                    Hided = true;
+                }
+               // this.Refresh();
+
+            }
+             
+
+            
+        }
+
+        private void testpanel_Paint(object sender, PaintEventArgs e)
+        {
 
         }
     }
