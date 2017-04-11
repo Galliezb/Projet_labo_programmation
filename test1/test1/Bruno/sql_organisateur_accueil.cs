0a8f058fe04b3bbd7ce6aeba5b7a60d5ab4c122d
Galliez Bruno <galliez.bruno@gmail.com>
2017-04-11 19:13:11 +0200

    test brubur

---
+++ test1/test1/Bruno/sql_organisateur_accueil.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1.Bruno {
+    class sql_organisateur_accueil {
+    }
+}
